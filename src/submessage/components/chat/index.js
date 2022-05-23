748443a41aa05089a3ebe2384f16c9f4104b3060
Lim Chee Kin <limcheekin@vobject.com>
2022-05-23 14:55:08 +0800

    feat: added icon, app name and tag line to chat component

--- src/submessage/components/chat/index.js
+++ src/submessage/components/chat/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Container, Divider } from 'semantic-ui-react'
+import { Container, Divider, Segment, Header, Icon } from 'semantic-ui-react'
 import MessageHeader from './MessageHeader'
 import MessageBody from './MessageBody'
 import MessageFooter from './MessageFooter'
@@ -7,13 +7,6 @@ import MessageFooter from './MessageFooter'
 // REF: https://ordinarycoders.com/blog/article/react-chakra-ui
 const Chat = () => {
   const [messages, setMessages] = useState([
-    { from: "computer", text: "Hi, My Name is HoneyChat" },
-    { from: "me", text: "Hey there" },
-    { from: "me", text: "Myself Ferin Patel" },
-    {
-      from: "computer",
-      text: "Nice to meet you. You can send me message and i'll reply you with same message.",
-    },
   ])
   const [inputMessage, setInputMessage] = useState("")
 
@@ -31,18 +24,29 @@ const Chat = () => {
     }, 1000)
   }
 
-  return (
-    <Container>
-      <MessageHeader />
-      <Divider />
-      <MessageBody messages={messages} />
-      <Divider />
-      <MessageFooter
-        inputMessage={inputMessage}
-        setInputMessage={setInputMessage}
-        handleSendMessage={handleSendMessage} />
-    </Container>
-  )
+  if (!messages.length) {
+    return (
+      <Segment placeholder>
+        <Header icon>
+          <Icon name="conversation">SubMessage</Icon>
+          Your Private and Secure Messenger
+        </Header>
+      </Segment>
+    )
+  } else {
+    return (
+      <Container>
+        <MessageHeader />
+        <Divider />
+        <MessageBody messages={messages} />
+        <Divider />
+        <MessageFooter
+          inputMessage={inputMessage}
+          setInputMessage={setInputMessage}
+          handleSendMessage={handleSendMessage} />
+      </Container>
+    )
+  }
 }
 
 export default Chat
