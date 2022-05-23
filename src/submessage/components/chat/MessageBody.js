4f93cd035614e235c922c203694993db250b61e7
Lim Chee Kin <limcheekin@vobject.com>
2022-05-23 10:54:44 +0800

    bug: fixed each child in a list should have unique key

--- src/submessage/components/chat/MessageBody.js
+++ src/submessage/components/chat/MessageBody.js
@@ -13,13 +13,13 @@ const MessageBody = ({ messages }) => {
       {messages.map((item, index) => {
         if (item.from === "me") {
           return (
-            <Container textAlign="right">
-              <Message id={index} compact color="black" style={messageStyle}>{item.text}</Message>
+            <Container textAlign="right" key={index}>
+              <Message compact color="black" style={messageStyle}>{item.text}</Message>
             </Container>
           )
         } else {
           return (
-            <Grid>
+            <Grid key={index}>
               <Grid.Row>
                 <Grid.Column width={1}>
                   <Avatar name="John Dow" round="20px" size="40" />
