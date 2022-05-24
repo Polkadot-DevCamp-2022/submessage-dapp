bfd55948cf9e85880a82945e6067266304dc3fae
Lim Chee Kin <limcheekin@vobject.com>
2022-05-24 13:59:48 +0800

    feat: custom div style for receiver message and added messageContainerStyle

--- src/submessage/components/chat/MessageBody.js
+++ src/submessage/components/chat/MessageBody.js
@@ -1,4 +1,4 @@
-import { Container, Message, Grid } from 'semantic-ui-react'
+import { Container, Message } from 'semantic-ui-react'
 import Avatar from 'react-avatar'
 import { u8aToString } from '@polkadot/util';
 import { naclDecrypt } from '@polkadot/util-crypto';
@@ -6,34 +6,35 @@ import { naclDecrypt } from '@polkadot/util-crypto';
 
 const MessageBody = ({ messages, sender, recipientName, commonKey }) => {
   const messageStyle = {
-    marginTop: '.3rem',
-    marginBottom: '.3rem',
     padding: '.5rem'
+  }
+
+  const messageContainerStyle = {
+    marginTop: '1rem',
+    marginBottom: '1rem',
   }
 
   return (
     <Container style={{ overflowY: "auto", height: "400px" }}>
-      { messages.map(message => {
+      {messages.map(message => {
         const isSender = message.sender.toString() === sender;
         const text = u8aToString(naclDecrypt(message.content.asEncrypted, message.nonce, commonKey))
         if (isSender) {
           return (
-            <Container textAlign="right" key={message.id}>
+            <Container textAlign="right" key={message.id} style={messageContainerStyle}>
               <Message compact color="black" style={messageStyle}>{text}</Message>
             </Container>
           )
         } else {
           return (
-            <Grid key={message.id}>
-              <Grid.Row>
-                <Grid.Column width={1}>
-                  <Avatar name={recipientName} round="20px" size="40" />
-                </Grid.Column>
-                <Grid.Column width={15}>
-                  <Message compact color="teal" style={messageStyle}>{text}</Message>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
+            <Container key={message.id} style={messageContainerStyle}>
+              <div style={{ width: "45px", float: "left", verticalAlign: "middle" }}>
+                <Avatar name={recipientName} round="20px" size="40" />
+              </div>
+              <div style={{ marginLeft: "45px" }}>
+                <Message compact color="teal" style={messageStyle}>{text}</Message>
+              </div>
+            </Container>
           )
         }
       })}
